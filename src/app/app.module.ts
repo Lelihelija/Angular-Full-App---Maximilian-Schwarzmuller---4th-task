f05062f5ae467e8ace4c1a59f60a865431d9a58b
Lelihelija <lelihelija@gmail.com>
2021-09-10 13:10:28 +0300

    firestire and stoptraining fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFireModule } from 'angularfire2';
@@ -13,7 +12,6 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { environment } from '../environments/environment';
@@ -27,8 +25,7 @@ import { TrainingModule } from './training/training.module';
     AppComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent,
-    StopTrainingComponent
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -37,13 +34,11 @@ import { TrainingModule } from './training/training.module';
     AppRoutingModule,
     FlexLayoutModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     AngularFireAuthModule,
     AuthModule,
     TrainingModule
   ],
   providers: [AuthService, TrainingService, UIService],
-  bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,23 +1,33 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { FormsModule } from '@angular/forms'
 import { MaterialModule } from '../material.module'
 
 // components
 import { CurrentTrainingComponent } from './current-training/current-training.component'
-import { NewTrainingComponent } from './new-training/new-training.component'
+import { StopTrainingComponent } from './current-training/stop-training.component'
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component'
+import { NewTrainingComponent } from './new-training/new-training.component'
 import { TrainingComponent } from './training.component'
 
 @NgModule({
   declarations: [
     CurrentTrainingComponent,
     PastTrainingsComponent,
+    StopTrainingComponent,
     NewTrainingComponent,
     TrainingComponent,
   ],
-  imports: [FormsModule, MaterialModule, FlexLayoutModule, CommonModule],
+  imports: [
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    CommonModule,
+    AngularFirestoreModule,
+  ],
   exports: [],
+  entryComponents: [StopTrainingComponent],
 })
 export class TrainingModule {}
